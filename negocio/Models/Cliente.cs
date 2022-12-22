9c46836cc476c290aab83235216364054a53f4f9
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2022-12-21 21:56:19 -0300

    excluir e incluir no razor

--- negocio/Models/Cliente.cs
+++ negocio/Models/Cliente.cs
@@ -81,4 +81,18 @@ public class Cliente
 
         return clientes;
     }
+
+    public static void ApagaPorId(int id)
+    {
+        using(var conn = new MySqlConnection(conexao))
+        {
+            conn.Open();
+            var query = $"delete from clientes where id = {id};";
+
+            var command = new MySqlCommand(query, conn);
+            command.ExecuteNonQuery();
+
+            conn.Close();
+        }
+    }
 }
--- web/Controllers/ClientesController.cs
+++ web/Controllers/ClientesController.cs
@@ -1,6 +1,6 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using web.Models;
+using Negocio.Models;
 
 namespace web.Controllers;
 
@@ -8,6 +8,32 @@ public class ClientesController : Controller
 {
     public IActionResult Index()
     {
+        ViewBag.clientes = Cliente.Todos();
         return View();
+    }
+
+    public IActionResult Novo()
+    {
+        return View();
+    }
+
+    public IActionResult Cadastrar([FromForm] Cliente cliente)
+    {
+        if(string.IsNullOrEmpty(cliente.Nome))
+        {
+            ViewBag.erro = "O nome não pode ser vazio";
+            return View();
+        }
+
+        cliente.Salvar();
+
+        return Redirect("/clientes");
+    }
+
+    [Route("/clientes/{id}/deletar")]
+    public IActionResult Apagar([FromRoute] int id)
+    {
+        Cliente.ApagaPorId(id);
+        return Redirect("/clientes");
     }
 }
